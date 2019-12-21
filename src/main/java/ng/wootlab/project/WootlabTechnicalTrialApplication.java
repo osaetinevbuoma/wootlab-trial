589f286ccfc60c2c3e556411848c70148870bbfc
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2019-12-21 13:20:14 +0100

    Initial setup

---
+++ src/main/java/ng/wootlab/project/WootlabTechnicalTrialApplication.java
@@ -0,0 +1,13 @@
+package ng.wootlab.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WootlabTechnicalTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WootlabTechnicalTrialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ng/wootlab/project/WootlabTechnicalTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.wootlab.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WootlabTechnicalTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
